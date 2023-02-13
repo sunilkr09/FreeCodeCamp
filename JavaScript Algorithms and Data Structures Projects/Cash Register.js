819a5648b95a69e5114595615e4b7226eca34972
sunilkr09 <93273505+sunilkr09@users.noreply.github.com>
2023-02-13 08:47:58 +0000

    Cash Register Completed

--- JavaScript Algorithms and Data Structures Projects/Cash Register.js
+++ JavaScript Algorithms and Data Structures Projects/Cash Register.js
@@ -1,17 +1,41 @@
-function checkCashRegister(price, cash, cid = [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) {
-    let totalCID = Math.round(cid.map(unit => unit[1]).reduce((sum, cash) => sum + cash) * 100) / 100;
-    let change = cash - price;
-    console.log(totalCID);
-    console.log(change)
-    if (change > totalCID) {
-        return { status: "INSUFFICIENT_FUNDS", change: [] }
-    } else if(change === totalCID){
-        return {status: "CLOSED", change: cid}
-    } else {
-        return null//{status: "OPEN", change: [...]}
-    }
-
-    return change;
+const currencyUnit = {
+    0.01: "PENNY",
+    0.05: "NICKEL",
+    0.1: "DIME",
+    0.25: "QUARTER",
+    1: "ONE",
+    5: "FIVE",
+    10: "TEN",
+    20: "TWENTY",
+    100: "ONE HUNDRED"
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+function checkCashRegister(price, cash, cid) {
+    let totalCID = Math.round(cid.map(unit => unit[1]).reduce((sum, cash) => sum + cash) * 100) / 100;
+    let changeInMachine = {};
+    cid.forEach(entity => changeInMachine[entity[0]] = entity[1]);
+    let change = cash - price;
+    if (change > totalCID) {
+        return { status: "INSUFFICIENT_FUNDS", change: [] }
+    } else if (change === totalCID) {
+        return { status: "CLOSED", change: cid }
+    } else {
+        let returnChange = { status: "OPEN", change: [] };
+        let currency = Object.keys(currencyUnit).sort((a, b) => b - a).map(c => Number.parseFloat(c));
+        currency.forEach(c => {
+            let totalChange = 0;
+            while (change >= c && totalChange < changeInMachine[currencyUnit[c]]) {
+                totalChange += c;
+                change -= c;
+                change = Math.round(change * 100) / 100;
+            }
+            if (totalChange)
+                returnChange.change.push([currencyUnit[c], totalChange]);
+        });
+
+        if(change != 0) return { status: "INSUFFICIENT_FUNDS", change: [] };
+        return returnChange;
+    }
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
